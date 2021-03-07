57695508ef67f82c84339fb59ec381d2342a1212
xxiex123 <y-ym1taiwan@hotmail.com>
2021-03-08 00:59:44 +0800

    Complete the function in queue.c but not sort.

--- queue.c
+++ queue.c
@@ -5,99 +5,176 @@
 #include "harness.h"
 #include "queue.h"
 
-/*
- * Create empty queue.
- * Return NULL if could not allocate space.
- */
 queue_t *q_new()
 {
     queue_t *q = malloc(sizeof(queue_t));
-    /* TODO: What if malloc returned NULL? */
-    q->head = NULL;
+    if(q == NULL){
+    printf("malloc fail.\n");
+    return NULL;
+    }q->head = q->tail = NULL;
+    q->size = 0;
     return q;
 }
 
-/* Free all storage used by queue */
 void q_free(queue_t *q)
 {
-    /* TODO: How about freeing the list elements and the strings? */
-    /* Free queue structure */
+    if(q == NULL)
+	return;
+    else if(q->head == NULL){
+	free(q);
+        return;
+    }
+
+    list_ele_t *temp;
+    temp = malloc(sizeof(list_ele_t));
+    if(temp == NULL){
+    printf("malloc error.");
+    return;
+    }
+    
+    temp = q->head->next;
+    free(q->head->value);
+    free(q->head);
+    while(temp){
+    q->head = temp;
+    temp = temp->next;
+    free(q->head->value);
+    free(q->head); 
+    }
     free(q);
 }
 
-/*
- * Attempt to insert element at head of queue.
- * Return true if successful.
- * Return false if q is NULL or could not allocate space.
- * Argument s points to the string to be stored.
- * The function must explicitly allocate space and copy the string into it.
- */
 bool q_insert_head(queue_t *q, char *s)
 {
+    if(q == NULL){
+    printf("q is NULL.");
+    return false;
+    }
+
     list_ele_t *newh;
-    /* TODO: What should you do if the q is NULL? */
     newh = malloc(sizeof(list_ele_t));
-    /* Don't forget to allocate space for the string and copy it */
-    /* What if either call to malloc returns NULL? */
+    
+    if(newh == NULL){
+    printf("malloc error.");
+    return false;
+    }newh->value = malloc(sizeof(char) * (strlen(s)+1));
+    
+    if(newh->value == NULL){
+    printf("malloc error");
+    return false;
+    }strcpy(newh->value,s);
     newh->next = q->head;
+    
+    if(q==NULL){
     q->head = newh;
+    q->tail = newh;
+    }else{
+    q->head = newh;
+    }
+    q->size++;
     return true;
 }
 
-/*
- * Attempt to insert element at tail of queue.
- * Return true if successful.
- * Return false if q is NULL or could not allocate space.
- * Argument s points to the string to be stored.
- * The function must explicitly allocate space and copy the string into it.
- */
 bool q_insert_tail(queue_t *q, char *s)
 {
-    /* TODO: You need to write the complete code for this function */
-    /* Remember: It should operate in O(1) time */
-    /* TODO: Remove the above comment when you are about to implement. */
-    return false;
-}
+    if(q == NULL)
+        return false;
 
-/*
- * Attempt to remove element from head of queue.
- * Return true if successful.
- * Return false if queue is NULL or empty.
- * If sp is non-NULL and an element is removed, copy the removed string to *sp
- * (up to a maximum of bufsize-1 characters, plus a null terminator.)
- * The space used by the list element and the string should be freed.
- */
-bool q_remove_head(queue_t *q, char *sp, size_t bufsize)
-{
-    /* TODO: You need to fix up this code. */
-    /* TODO: Remove the above comment when you are about to implement. */
-    q->head = q->head->next;
+    list_ele_t *newh;
+    newh = malloc(sizeof(list_ele_t));
+    
+    if(newh == NULL){
+    printf("malloc error.");
+    return false;
+    }newh->value = malloc(sizeof(char) * (strlen(s)+1));
+    
+    if(newh->value == NULL){
+    printf("malloc error");
+    return false;
+    }strcpy(newh->value,s);
+    newh->next = NULL;
+    
+    if(q->head == NULL){
+    q->head = newh;
+    q->tail = newh;
+    }else{
+    q->tail->next = newh;
+    q->tail = newh;
+    }
+    q->size++;
     return true;
 }
 
-/*
- * Return number of elements in queue.
- * Return 0 if q is NULL or empty
- */
-int q_size(queue_t *q)
+bool q_remove_head(queue_t *q, char *sp, size_t bufsize)
 {
-    /* TODO: You need to write the code for this function */
-    /* Remember: It should operate in O(1) time */
-    /* TODO: Remove the above comment when you are about to implement. */
-    return 0;
+    if(q == NULL){
+    printf("q is NULL.");
+    return false;    
+    }else if(q->head == NULL){
+    printf("q is empty.")
+    return false;
+    }
+  
+    list_ele_t *temp;
+    temp = malloc(sizeof(list_ele_t));
+    if(temp == NULL){
+    printf("malloc error.");
+    return false;
+    }
+
+    if(sp != NULL){
+    for(int i = 0; i < bufsize-1 ; i++)
+	sp[i] = q->head->value[i];
+    sp[bufsize] = '\0';
+    }
+    free(q->head->value);
+    
+    temp = q->head->next;
+    q->head->next = NULL;
+    q->head = temp;
+    if(q->head == NULL)
+	q->tail == NULL;
+    return true;
 }
 
-/*
- * Reverse elements in queue
- * No effect if q is NULL or empty
- * This function should not allocate or free any list elements
- * (e.g., by calling q_insert_head, q_insert_tail, or q_remove_head).
- * It should rearrange the existing ones.
- */
+int q_size(queue_t *q)
+{
+    if(q == NULL){
+    printf("q is NULL.");
+    return 0;
+    }else if(q->head == NULL){
+    printf("q is empty.");
+    return 0;
+    }
+
+    return q->size;
+}
+
 void q_reverse(queue_t *q)
 {
-    /* TODO: You need to write the code for this function */
-    /* TODO: Remove the above comment when you are about to implement. */
+    if(q == NULL)
+        return;
+    else if(q->head == NULL)
+	return;
+    
+    list_ele_t *tempOne , *tempTwo;
+    tempOne = malloc(sizeof(list_ele_t));
+    tempTwo = malloc(sizeof(list_ele_t));
+    if(tempOne == NULL || tempTwo == NULL){
+    printf("malloc error.");
+    return;
+    }
+
+    tempOne = q->head->next;
+    q->head->next = NULL;
+    q->tail = tempTwo = q->head;
+    while(tempOne){
+    q->head = temp;
+    tempOne = temp->next;
+    q->head->next = tempTwo;
+    tempTwo = q->head;
+    }
+
 }
 
 /*
@@ -107,6 +184,16 @@ void q_reverse(queue_t *q)
  */
 void q_sort(queue_t *q)
 {
+    if(q == NULL){
+        return;
+    }else if(q->head == NULL){
+        return;
+    }else if(q->head->next == NULL){
+	return;
+    }
+
+
+
     /* TODO: You need to write the code for this function */
     /* TODO: Remove the above comment when you are about to implement. */
 }
